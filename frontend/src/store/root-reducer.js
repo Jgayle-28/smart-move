0c90463a89bca8cbebc8354982016284d41159ad
Jerehme Gayle <jerehme.gayle@gmail.com>
2025-01-01 20:05:46 -0700

    chore: cleaned up redux store & slices

--- frontend/src/store/root-reducer.js
+++ frontend/src/store/root-reducer.js
@@ -1,9 +1,4 @@
 import { combineReducers } from '@reduxjs/toolkit'
-import { reducer as calendarReducer } from 'src/slices/calendar'
-import { reducer as chatReducer } from 'src/slices/chat'
-import { reducer as kanbanReducer } from 'src/slices/kanban'
-import { reducer as mailReducer } from 'src/slices/mail'
-// NEW
 import authReducer from './auth/authSlice'
 import companyReducer from './company/companySlice'
 import customerReducer from './customers/customerSlice'
@@ -20,9 +15,4 @@ export const rootReducer = combineReducers({
   estimates: estimateReducer,
   inventory: inventoryReducer,
   payments: paymentReducer,
-  // OLD
-  calendar: calendarReducer,
-  chat: chatReducer,
-  kanban: kanbanReducer,
-  mail: mailReducer,
 })
