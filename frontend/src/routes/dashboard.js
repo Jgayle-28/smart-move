377105968864688f5d9d4e987ccbd2f85c96c2fe
Jerehme Gayle <jerehme.gayle@gmail.com>
2025-01-03 21:02:40 -0700

    chore: changed job list naming

--- frontend/src/routes/dashboard.js
+++ frontend/src/routes/dashboard.js
@@ -23,8 +23,8 @@ const InvoiceDetailPage = lazy(() =>
 )
 
 // Jobs
-import JobsList from '../pages/dashboard/jobs/List.jsx'
-// const JobsList = lazy(() => import('src/pages/dashboard/jobs/List'))
+
+const JobsList = lazy(() => import('src/pages/dashboard/jobs/list'))
 const JobCreatePage = lazy(() => import('src/pages/dashboard/jobs/create'))
 const JobDetailPage = lazy(() => import('src/pages/dashboard/jobs/detail'))
 // Estimates
